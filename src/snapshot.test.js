ebe2c4e3101ffd83be77e30409da96b1b4fa9ae1
Alejandro Iglesias <alexiglesias3110@gmail.com>
2021-06-24 12:35:16 -0400

    Test working

--- src/snapshot.test.js
+++ src/snapshot.test.js
@@ -1,4 +1,4 @@
-const { puppeteer } = require('puppeteer');
+const puppeteer = require('puppeteer');
 const { toMatchImageSnapshot } = require('jest-image-snapshot');
 expect.extend({ toMatchImageSnapshot });
 
