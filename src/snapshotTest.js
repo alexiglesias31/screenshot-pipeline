bfd05d3525e36d640a51cf165f779b993688302c
Alejandro Iglesias <alexiglesias3110@gmail.com>
2021-06-24 11:44:46 -0400

    Jest test implemented, puppeteer installed

---
+++ src/snapshotTest.js
@@ -0,0 +1,12 @@
+const { puppeteer } = require('puppeteer');
+const { toMatchImageSnapshot } = require('jest-image-snapshot');
+expect.extend({ toMatchImageSnapshot });
+
+it('renders correctly', async () => {
+    const browser = await puppeteer.launch({ headless: true })
+    const page = await browser.newPage();
+    await page.goto('https://localhost:3000');
+    const image = await page.screenshot();
+  
+    expect(image).toMatchImageSnapshot();
+  });
